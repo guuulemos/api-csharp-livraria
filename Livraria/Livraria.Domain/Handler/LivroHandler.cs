2c739025187a19ac7f625d6032a0ca018e55c987
Gustavo Lemos <contato.gustavolemos@outlook.com>
2021-10-19 11:21:50 -0300

    Api livraria

---
+++ Livraria/Livraria.Domain/Handler/LivroHandler.cs
@@ -0,0 +1,110 @@
+﻿using Flunt.Notifications;
+using Livraria.Domain.Commands.Inputs;
+using Livraria.Domain.Commands.Outputs;
+using Livraria.Domain.Entidades;
+using Livraria.Domain.Interfaces.Repositories;
+using Livraria.Infra.Interfaces.Commands;
+using System;
+
+namespace Livraria.Domain.Handler
+{
+    public class LivroHandler : ICommandHandler<AdicionarLivroCommand>, ICommandHandler<AtualizarLivroCommand>, ICommandHandler<ExcluirLivroCommand>
+    {
+        private readonly ILivroRepository _repository;
+
+        public LivroHandler(ILivroRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ICommandResult Handle(AdicionarLivroCommand command)
+        {
+            try
+            {
+                if (!command.ValidarCommand())
+                    return new LivroCommandResult(false, "Por favor corrija as inconsistências abaixo", command.Notifications);
+
+                long id = 0;
+                string nome = command.Nome;
+                string autor = command.Autor;
+                int edicao = command.Edicao;
+                string isbn = command.Isbn;
+                string imagem = command.Imagem;
+
+                Livro livro = new Livro(id, nome, autor, edicao, isbn, imagem);
+
+                id = _repository.Inserir(livro);
+
+                return new LivroCommandResult(true, "Livro adicionado com sucesso!", new
+                {
+                    Nome = livro.Nome,
+                    Autor = livro.Autor,
+                    Edicao = livro.Edicao,
+                    Isbn = livro.Isbn,
+                    Imagem = livro.Imagem
+                });
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+        }
+
+        public ICommandResult Handle(AtualizarLivroCommand command)
+        {
+            try
+            {
+                if (!command.ValidarCommand())
+                    return new LivroCommandResult(false, "Por favor corrija as inconsistências abaixo", command.Notifications);
+
+                if(!_repository.CheckId(command.Id))
+                    return new LivroCommandResult(false, "Id", new Notification("Id", "Id inválido. Este id não está cadastrado!"));
+
+                long id = command.Id;
+                string nome = command.Nome;
+                string autor = command.Autor;
+                int edicao = command.Edicao;
+                string isbn = command.Isbn;
+                string imagem = command.Imagem;
+
+                Livro livro = new Livro(id, nome, autor, edicao, isbn, imagem);
+
+                _repository.Atualizar(livro);
+
+                return new LivroCommandResult(true, "Livro atualizado com sucesso!", new
+                {
+                    Id = livro.Id,
+                    Nome = livro.Nome,
+                    Autor = livro.Autor,
+                    Edicao = livro.Edicao,
+                    Isbn = livro.Isbn,
+                    Imagem = livro.Imagem
+                });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public ICommandResult Handle(ExcluirLivroCommand command)
+        {
+            try
+            {
+                if (!command.ValidarCommand())
+                    return new LivroCommandResult(false, "Por favor corrija as inconsistências abaixo", command.Notifications);
+
+                if (!_repository.CheckId(command.Id))
+                    return new LivroCommandResult(false, "Id", new Notification("Id", "Id inválido. Este id não está cadastrado!"));
+
+                _repository.Excluir(command.Id);
+
+                return new LivroCommandResult(true, "Livro excluído com sucesso!", new { Id = command.Id });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Livraria/LivrariaApi/Controllers/LivroController.cs
@@ -0,0 +1,62 @@
+﻿using Livraria.Domain.Commands.Inputs;
+using Livraria.Domain.Handler;
+using Livraria.Domain.Interfaces.Repositories;
+using Livraria.Domain.Query;
+using Livraria.Infra.Interfaces.Commands;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LivrariaApi.Controllers
+{
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        private readonly ILivroRepository _repository;
+        private readonly LivroHandler _handler;
+
+        public LivroController(ILivroRepository repository, LivroHandler handler)
+        {
+            _repository = repository;
+            _handler = handler;
+        }
+
+        [HttpGet]
+        [Route("v1/livro/{id}")]
+        public LivroQueryResult ObterLivro(long id)
+        {
+            return _repository.Obter(id);
+        }
+
+        [HttpGet]
+        [Route("v1/livro")]
+        public List<LivroQueryResult> ListarLivros()
+        {
+            return _repository.Listar();
+        }
+
+        [HttpPost]
+        [Route("v1/livro")]
+        public ICommandResult Inserir([FromBody] AdicionarLivroCommand command)
+        {
+            return _handler.Handle(command);
+        }
+
+        [HttpPut]
+        [Route("v1/livro/{id}")]
+        public ICommandResult Atualizar(long id, [FromBody] AtualizarLivroCommand command)
+        {
+            command.Id = id;
+            return _handler.Handle(command);
+        }
+
+        [HttpDelete]
+        [Route("v1/livro/{id}")]
+        public ICommandResult Excluir(long id)
+        {
+            var command = new ExcluirLivroCommand() { Id = id};
+            return _handler.Handle(command);
+        }
+    }
+}
